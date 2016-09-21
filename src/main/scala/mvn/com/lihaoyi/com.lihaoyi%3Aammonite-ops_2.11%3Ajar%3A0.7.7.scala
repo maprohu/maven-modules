ac32818c2d7c99e7b242da71c2723dce04485b4d
maprohu <maprohu@gmail.com>
2016-09-21 12:02:41 +0100

    ftx

---
+++ src/main/scala/mvn/com/lihaoyi/com.lihaoyi%3Aammonite-ops_2.11%3Ajar%3A0.7.7.scala
@@ -0,0 +1,10 @@
+
+package mvn
+
+object `com.lihaoyi:ammonite-ops_2.11:jar:0.7.7` extends _root_.jartree.util.CaseClassLoaderKey(
+  jar = _root_.jartree.util.MavenJarKeyImpl("com.lihaoyi:ammonite-ops_2.11:jar:0.7.7"),
+  dependenciesSeq = collection.immutable.Seq(
+    `org.scala-lang:scala-library:jar:2.11.8`
+  )
+)
+         
---
+++ src/main/scala/mvn/maprohu/maprohu%3Ascala-ext-common%3Ajar%3A1.0.3.scala
@@ -0,0 +1,10 @@
+
+package mvn
+
+object `maprohu:scala-ext-common:jar:1.0.3` extends _root_.jartree.util.CaseClassLoaderKey(
+  jar = _root_.jartree.util.MavenJarKeyImpl("maprohu:scala-ext-common:jar:1.0.3"),
+  dependenciesSeq = collection.immutable.Seq(
+    `org.scala-lang:scala-library:jar:2.11.8`
+  )
+)
+         
