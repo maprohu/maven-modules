def379957770912e24ff812bd482a54ecc390997
maprohu <maprohu@gmail.com>
2016-09-22 12:19:35 +0100

    ftx

---
+++ src/main/scala/mvn/com/sun/xml/bind/com.sun.xml.bind%3Ajaxb-impl%3Ajar%3A2.2.11.scala
@@ -0,0 +1,10 @@
+
+package mvn
+
+object `com.sun.xml.bind:jaxb-impl:jar:2.2.11` extends _root_.jartree.util.CaseClassLoaderKey(
+  jar = _root_.jartree.util.MavenJarKeyImpl("com.sun.xml.bind:jaxb-impl:jar:2.2.11"),
+  dependenciesSeq = collection.immutable.Seq(
+    
+  )
+)
+         
---
+++ src/main/scala/mvn/com/sun/xml/bind/com.sun.xml.bind%3Ajaxb-xjc%3Ajar%3A2.2.11.scala
@@ -0,0 +1,10 @@
+
+package mvn
+
+object `com.sun.xml.bind:jaxb-xjc:jar:2.2.11` extends _root_.jartree.util.CaseClassLoaderKey(
+  jar = _root_.jartree.util.MavenJarKeyImpl("com.sun.xml.bind:jaxb-xjc:jar:2.2.11"),
+  dependenciesSeq = collection.immutable.Seq(
+    
+  )
+)
+         
