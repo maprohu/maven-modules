dc7a600a81260b0c55af41d32d0329480912105a
maprohu <maprohu@gmail.com>
2016-09-23 17:53:42 +0100

    ftx

---
+++ src/main/scala/mvn/com/h2database/com.h2database%3Ah2%3Ajar%3A1.4.192.scala
@@ -0,0 +1,10 @@
+
+package mvn
+
+object `com.h2database:h2:jar:1.4.192` extends _root_.jartree.util.CaseClassLoaderKey(
+  jar = _root_.jartree.util.MavenJarKeyImpl("com.h2database:h2:jar:1.4.192"),
+  dependenciesSeq = collection.immutable.Seq(
+    
+  )
+)
+         
