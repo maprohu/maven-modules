990d5e28f1ccf78bf33b9f61db93617634b06d86
maprohu <maprohu@gmail.com>
2016-10-01 20:31:35 +0100

    maven modules

---
+++ builder/src/main/scala/maven/modules/builder/Module.scala
@@ -0,0 +1,592 @@
+package maven.modules.builder
+
+import java.io.File
+
+import jartree.util.{CaseClassLoaderKey, CaseJarKey, HashJarKeyImpl, MavenJarKeyImpl}
+import maven.modules.builder.Module.DeployableModule
+import org.eclipse.aether.util.version.GenericVersionScheme
+import org.eclipse.aether.version.Version
+import sbt.io.IO
+
+import scala.xml.PrettyPrinter
+
+/**
+  * Created by pappmar on 29/08/2016.
+  */
+
+//trait Module {
+//  def artifactId(contextGroupId: String, place: Option[Pom]) : String
+//  def groupId(contextGroupId: String) : String
+//  def version : String
+//  def classifier : Option[String]
+//  def deps: Seq[Module]
+//}
+//
+//
+//
+//class ExternalModule(
+//  val groupId: String,
+//  val artifactId: String,
+//  val version: String,
+//  val classifier: Option[String],
+//  val deps: Module*
+//) extends Module {
+//  override def groupId(contextGroupId: String): String = groupId
+//  override def artifactId(contextGroupId: String, place: Option[Pom]): String = artifactId
+//}
+//
+//case class InternalModule(
+//  module: NamedModule
+//) extends Module {
+//  override def groupId(contextGroupId: String): String = contextGroupId
+//  override def artifactId(contextGroupId: String, place: Option[Pom]): String = s"${place.get.artifactId(contextGroupId)}-${module.name}"
+//  override def version: String = module.version
+//  override def classifier: Option[String] = None
+//  override def deps: Seq[Module] = module.deps
+//}
+
+trait ModuleId
+trait ModuleVersion extends Comparable[ModuleVersion] {
+  def moduleId : ModuleId
+}
+
+case class MavenModuleId(
+  groupId: String,
+  artifactId: String,
+  classifier: Option[String]
+) extends ModuleId
+case class HashModuleId(
+  hash: Seq[Byte]
+) extends ModuleId
+
+case class HashModuleVersion(
+  moduleId: HashModuleId
+) extends ModuleVersion {
+  override def compareTo(o: ModuleVersion): Int = {
+    require(o.asInstanceOf[HashModuleVersion].moduleId == moduleId)
+    0
+  }
+}
+case class MavenModuleVersion(
+  mavenModuleId: MavenModuleId,
+  version: Version
+) extends ModuleVersion {
+  override def compareTo(o: ModuleVersion): Int = {
+    val mmv = o.asInstanceOf[MavenModuleVersion]
+    require(moduleId == mmv.moduleId)
+    version.compareTo(mmv.version)
+  }
+  override def moduleId: MavenModuleId = mavenModuleId
+}
+
+case class Module(
+  val version: ModuleVersion,
+  val deps: Seq[Module],
+  val provided : Boolean = false
+)
+
+
+//trait ModuleDir {
+//  def parent: Option[(ModuleDir, String)]
+//}
+
+object Module {
+//  implicit def classLoaderKey2Module(clk: ClassLoaderKey) : Module = {
+//    new ExternalModule(
+//      clk.jar.groupId,
+//      clk.jar.artifactId,
+//      clk.jar.version,
+//      clk.jar.classifier,
+//      clk.parents.map(classLoaderKey2Module):_*
+//    )
+//  }
+//
+//  implicit def namedModuleToModule(namedModule: NamedModule) : InternalModule = {
+//    new InternalModule(
+//      namedModule
+//    )
+//  }
+  def provided(module: Module) : Module = new Module(
+    module.version,
+    module.deps,
+    true
+  )
+
+  val versionScheme = new GenericVersionScheme
+
+  def jarKey2ModuleIdVersion(jarKey: CaseJarKey) : ModuleVersion = {
+    jarKey match {
+      case m : MavenJarKeyImpl =>
+        MavenModuleVersion(
+          MavenModuleId(
+            m.groupId,
+            m.artifactId,
+            m.classifierOpt
+          ),
+          versionScheme.parseVersion(m.version)
+        )
+      case h : HashJarKeyImpl =>
+        HashModuleVersion(
+          HashModuleId(
+            h.hashSeq
+          )
+        )
+
+    }
+
+
+  }
+
+  implicit def classLoaderKey2Module(clk: CaseClassLoaderKey) : Module = {
+    new Module(
+      jarKey2ModuleIdVersion(clk.jar),
+      clk.dependenciesSeq.map(classLoaderKey2Module)
+    )
+  }
+
+  implicit def namedModuleToModule(namedModule: NamedModule) : Module = {
+    new Module(
+      MavenModuleVersion(
+        MavenModuleId(
+          namedModule.container.root.groupId,
+          namedModule.path.mkString("-"),
+          None
+        ),
+        versionScheme.parseVersion(namedModule.version)
+      ),
+      namedModule.deps
+    )
+  }
+
+  type PlaceLookup = RootModuleContainer => File
+
+  def projectDir(
+    namedModule: NamedModule,
+    placeLookup: PlaceLookup
+  ) : File = {
+    namedModule
+      .path
+      .tail
+      .foldLeft(
+        placeLookup(namedModule.container.root)
+      )(
+        new File(_, _)
+      )
+  }
+
+  trait DeployableModule {
+    def groupId: String
+    def artifactId : String
+    def version: String
+  }
+
+  case class DeployableModuleImpl(
+    groupId: String,
+    artifactId : String,
+    version: String
+  ) extends DeployableModule
+
+  object DeployableModule {
+    implicit class Ops(module: DeployableModule) {
+      def toBundle : DeployableModule = {
+        DeployableModuleImpl(
+          groupId = module.groupId,
+          artifactId = s"${module.artifactId}-bundle",
+          version = module.version
+        )
+      }
+    }
+    implicit def key2deployable(key: CaseClassLoaderKey) : DeployableModule = key.jar match {
+      case m : MavenJarKeyImpl =>
+        DeployableModuleImpl(
+          m.groupId,
+          m.artifactId,
+          m.version
+        )
+      case _ => ???
+    }
+  }
+
+  def asPomCoordinates(
+    module: DeployableModule
+  ) = {
+    <groupId>{module.groupId}</groupId>
+    <artifactId>{module.artifactId}</artifactId>
+    <version>{module.version}</version>
+  }
+
+  def asPomDependency(
+    module: DeployableModule
+  ) = {
+    <dependency>
+      {asPomCoordinates(module)}
+    </dependency>
+  }
+
+  def generate(
+    roots: Seq[PlacedRoot],
+    modules: Seq[NamedModule]
+  ) : Unit = {
+    val containedContainers =
+      modules
+        .flatMap(_.container.toContainedSeq)
+        .distinct
+
+    val containedModules =
+      containedContainers ++ modules
+
+
+    val childrenMap =
+      containedModules
+        .groupBy(_.parent)
+
+    val pretty = new PrettyPrinter(300, 4)
+
+    val placeLookup : Map[RootModuleContainer, File] =
+      roots
+        .map(p => p.rootContainer -> p.rootDir)
+        .toMap
+
+    val containerModules =
+      modules
+        .flatMap(_.container.toSeq)
+        .distinct
+
+
+    containerModules
+      .foreach({ place =>
+        val dir = place
+          .toContainedSeq
+          .map(_.name)
+          .foldLeft(placeLookup(place.root))(new File(_, _))
+
+        dir.mkdirs()
+
+        val xml =
+          <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+            <modelVersion>4.0.0</modelVersion>
+            {
+//              place
+//                .parentOpt
+//                .map({ parent =>
+//                  <parent>
+//                    <groupId>{place.root.name}</groupId>
+//                    <artifactId>{parent.artifactId}</artifactId>
+//                    <version>1.0.0</version>
+//                  </parent>
+//                })
+//                .toSeq
+            }
+
+            <groupId>{place.root.name}</groupId>
+            <artifactId>{place.artifactId}</artifactId>
+            <version>1.0.0</version>
+            <packaging>pom</packaging>
+
+            <build>
+              <plugins>
+                <plugin>
+                  <groupId>org.apache.maven.plugins</groupId>
+                  <artifactId>maven-dependency-plugin</artifactId>
+                  <version>2.10</version>
+                </plugin>
+              </plugins>
+            </build>
+
+            <modules>
+              {
+                childrenMap
+                  .get(place)
+                  .toSeq
+                  .flatMap({ children =>
+                    children.map({ child =>
+                      <module>{child.name}</module>
+                    })
+                  })
+              }
+            </modules>
+          </project>
+
+        writeFile(
+          new File(dir, "pom.xml"),
+          pretty.format(xml)
+        )
+      })
+
+    modules
+      .foreach({ module =>
+        val dir =
+          new File(
+            module
+              .container
+              .toContainedSeq
+              .map(_.name)
+              .foldLeft(placeLookup(module.container.root))(new File(_, _)),
+            module.name
+          )
+        dir.mkdirs()
+
+        val xml =
+          <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+            <modelVersion>4.0.0</modelVersion>
+            <groupId>{module.container.root.groupId}</groupId>
+            {
+//            <parent>
+//              <groupId>{module.container.root.groupId}</groupId>
+//              <artifactId>{module.container.artifactId}</artifactId>
+//              <version>1.0.0</version>
+//            </parent>
+            }
+
+            <artifactId>{module.container.artifactId}-{module.name}</artifactId>
+            <version>{module.version}</version>
+            <packaging>jar</packaging>
+            <build>
+              <finalName>product</finalName>
+              <plugins>
+                <plugin>
+                  <groupId>net.alchim31.maven</groupId>
+                  <artifactId>scala-maven-plugin</artifactId>
+                  <version>3.2.1</version>
+                  <executions>
+                    <execution>
+                      <goals>
+                        <goal>add-source</goal>
+                        <goal>compile</goal>
+                        <goal>testCompile</goal>
+                      </goals>
+                    </execution>
+                  </executions>
+                </plugin>
+                <plugin>
+                  <groupId>org.apache.maven.plugins</groupId>
+                  <artifactId>maven-compiler-plugin</artifactId>
+                  <version>3.5.1</version>
+                  <configuration>
+                    <source>1.6</source>
+                    <target>1.6</target>
+                  </configuration>
+                </plugin>
+                <plugin>
+                  <groupId>org.apache.maven.plugins</groupId>
+                  <artifactId>maven-source-plugin</artifactId>
+                  <version>3.0.1</version>
+                  <executions>
+                    <execution>
+                      <id>attach-sources</id>
+                      <phase>package</phase>
+                      <goals>
+                        <goal>jar</goal>
+                      </goals>
+                    </execution>
+                  </executions>
+                </plugin>
+                <plugin>
+                  <groupId>org.apache.maven.plugins</groupId>
+                  <artifactId>maven-dependency-plugin</artifactId>
+                  <version>2.10</version>
+                </plugin>
+                <plugin>
+                  <groupId>org.codehaus.mojo</groupId>
+                  <artifactId>build-helper-maven-plugin</artifactId>
+                  <version>1.12</version>
+                  <executions>
+                    <execution>
+                      <id>add-source</id>
+                      <phase>generate-sources</phase>
+                      <goals>
+                        <goal>add-source</goal>
+                      </goals>
+                      <configuration>
+                        <sources>
+                          <source>{"${project.build.directory}/generated-sources"}</source>
+                        </sources>
+                      </configuration>
+                    </execution>
+                  </executions>
+                </plugin>
+              </plugins>
+            </build>
+            <dependencies>
+              {
+                module
+                  .deps
+                  .map(m => (m.version, m.provided))
+                  .collect({ case (dep : MavenModuleVersion, provided) =>
+                    <dependency>
+                      <groupId>{dep.moduleId.groupId}</groupId>
+                      <artifactId>{dep.moduleId.artifactId}</artifactId>
+                      <version>{dep.version.toString}</version>
+                      {dep.moduleId.classifier.map(c => <classifier>{c}</classifier>).toSeq}
+                      {if (provided) <scope>provided</scope> else Seq()}
+                    </dependency>
+                  })
+              }
+            </dependencies>
+          </project>
+
+        module.path.foldLeft(new File(dir, "src/main/scala"))(new File(_, _)).mkdirs()
+
+        writeFile(
+          new File(dir, "pom.xml"),
+          pretty.format(xml)
+        )
+      })
+  }
+
+  def writeFile(
+    file: File,
+    content: String
+  ) : Unit = {
+    if (!file.exists() || IO.read(file) != content) {
+      println(s"Writing: ${file}")
+      IO.write(file, content)
+    } else {
+      println(s"Skipping: ${file}")
+    }
+  }
+}
+
+sealed trait ContainedModule {
+  def parent: ModuleContainer
+  def name : String
+}
+
+class NamedModule(
+  val container: ModuleContainer,
+  val name: String,
+  val version: String,
+  val deps: Module*
+) extends ContainedModule with DeployableModule {
+  def path : Seq[String] = container.path :+ name
+  def parent: ModuleContainer = container
+  def groupId = container.root.groupId
+  def artifactId = path.mkString("-")
+  def pkg = path.mkString(".")
+}
+
+class JavaModule(
+  name: String,
+  version: String,
+  deps: Module*
+)(implicit
+  container: ModuleContainer
+) extends NamedModule (
+  container,
+  name,
+  version,
+  (deps ++ Seq[Module](
+    Module.provided(mvn.`org.scala-lang:scala-library:jar:2.11.8`)
+  )):_*
+)
+
+class ScalaModule(
+  name: String,
+  version: String,
+  deps: Module*
+)(implicit
+  container: ModuleContainer
+) extends NamedModule (
+  container,
+  name,
+  version,
+  (deps ++ Seq[Module](
+    mvn.`org.scala-lang:scala-library:jar:2.11.8`
+  )):_*
+)
+
+class PlacedRoot(
+  val rootContainer: RootModuleContainer,
+  val rootDir: File
+)
+
+object PlacedRoot {
+  implicit def fromTuple(
+    tuple: (RootModuleContainer, File)
+  ) = tuple match {
+    case (root, dir) =>
+      new PlacedRoot(root, dir)
+  }
+}
+//object NamedModule {
+//  def of(
+//    name: String,
+//    version: String,
+//    deps: Module*
+//  ): NamedModule = new NamedModule(
+//    new Module(version, deps:_*),
+//    name,
+//    version
+//  )
+//}
+
+//class PlacedModule(
+//  val place: Pom,
+//  val module: NamedModule
+//) extends ModuleDir {
+//  override def parent: Option[(ModuleDir, String)] = Some((place, module.name))
+//  def path : Seq[String] = place.path :+ module.name
+//}
+//
+//object PlacedModule {
+//  implicit def pairToPlacedModule(pair: (Pom, NamedModule)) : PlacedModule = pair match {
+//    case (place, module) =>
+//      new PlacedModule(place, module)
+//  }
+//}
+
+
+//case class Pom(
+//  parent: Option[(Pom, String)] = None
+//) extends ModuleDir {
+//  def toSeq : Seq[Pom] = {
+//    parent
+//      .map(_._1.toSeq)
+//      .toSeq
+//      .flatten :+ this
+//  }
+//
+//  def path : Seq[String] = toSeq.flatMap(_.parent.map(_._2))
+//
+//  def artifactId(groupId: String) : String = (groupId +: path).mkString("-")
+//}
+//
+//object Pom {
+//  def apply(parent: Pom, segment: String) : Pom = Pom(Some((parent, segment)))
+//}
+
+trait Artifact {
+  def artifactId : String
+}
+
+sealed trait ModuleContainer extends Artifact {
+  def name : String
+  def root : RootModuleContainer
+  def path : Seq[String] = toSeq.map(_.name)
+  def toSeq : Seq[ModuleContainer]
+  def toContainedSeq : Seq[ContainedModule]
+  def parentOpt: Option[ModuleContainer]
+  def artifactId: String = path.mkString("-")
+}
+
+case class RootModuleContainer(
+  groupId : String
+) extends ModuleContainer {
+  def name = groupId
+  def root = this
+  def toSeq: Seq[ModuleContainer] = Seq(this)
+  def toContainedSeq: Seq[ContainedModule] = Seq()
+  def parentOpt: Option[ModuleContainer] = None
+}
+
+case class SubModuleContainer(
+  parent: ModuleContainer,
+  name: String
+) extends ModuleContainer with ContainedModule {
+  def root = parent.root
+  def toSeq: Seq[ModuleContainer] = parent.toSeq :+ this
+  def toContainedSeq: Seq[ContainedModule] = parent.toContainedSeq :+ this
+  def parentOpt: Option[ModuleContainer] = Some(parent)
+}
+
+
---
+++ generator/src/main/scala/maven/modules/generator/RunGenerateMaven.scala
@@ -0,0 +1,155 @@
+package maven.modules.generator
+
+import java.io.File
+import java.net.URLEncoder
+
+import org.jboss.shrinkwrap.resolver.api.maven.{Maven, ScopeType}
+import sbt.io.IO
+
+import scala.util.control.NonFatal
+
+/**
+  * Created by martonpapp on 28/08/16.
+  */
+object RunGenerateMaven {
+
+  val artifacts = Seq(
+    "org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-api:jar:2.2.2",
+    "org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-spi:jar:2.2.2",
+    "org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-api-maven:jar:2.2.2",
+    "org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-spi-maven:jar:2.2.2",
+    "org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-impl-maven:jar:2.2.2",
+    "org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-impl-maven-archive:jar:2.2.2",
+    "com.typesafe.akka:akka-stream_2.11:2.4.9",
+    "com.typesafe.akka:akka-slf4j_2.11:2.3.15",
+    "javax.servlet:servlet-api:jar:2.5",
+    "javax.servlet:javax.servlet-api:jar:3.1.0",
+    "org.scala-lang:scala-library:jar:2.11.8",
+    "com.lihaoyi:scalarx_2.11:jar:0.3.1",
+    "commons-io:commons-io:jar:2.5",
+    "commons-codec:commons-codec:jar:1.10",
+    "jartree:jartree-api:jar:1.0.0-SNAPSHOT",
+    "jartree:jartree-impl:jar:1.0.0-SNAPSHOT",
+    "org.slf4j:slf4j-api:jar:1.7.21",
+    "org.slf4j:slf4j-simple:jar:1.7.21",
+    "com.typesafe.scala-logging:scala-logging_2.11:jar:3.4.0",
+    "com.typesafe.akka:akka-stream-experimental_2.11:jar:2.0.4",
+    "com.lihaoyi:upickle_2.11:jar:0.4.2",
+    "org.scala-sbt:io_2.11:jar:1.0.0-M6",
+    "com.jsuereth:scala-arm_2.11:jar:1.4",
+    "org.scala-lang.modules:scala-pickling_2.11:jar:0.10.1",
+    "com.typesafe.akka:akka-http-experimental_2.11:jar:2.4.9",
+    "org.webjars.bower:vis:jar:4.16.1",
+    "org.scala-js:scalajs-library_2.11:jar:0.6.12",
+    "org.scala-js:scalajs-dom_sjs0.6_2.11:jar:0.9.1",
+    "com.github.wendykierp:JTransforms:jar:3.1",
+    "org.scala-lang.modules:scala-swing_2.11:jar:2.0.0-M2",
+    "io.monix:monix_2.11:jar:2.0.2",
+    "de.heikoseeberger:akka-http-json4s_2.11:jar:1.9.0",
+    "org.json4s:json4s-native_2.11:jar:3.4.0",
+    "emsa:wupdata-common-shared:jar:1.0.4-SNAPSHOT",
+    "emsa:wupdata-core:jar:1.0.5-SNAPSHOT",
+    "com.github.nscala-time:nscala-time_2.11:jar:2.12.0",
+    "com.github.nscala-time:nscala-time_2.11:jar:2.14.0",
+    "io.github.lukehutch:fast-classpath-scanner:jar:2.0.3",
+    "org.osgi:org.osgi.core:jar:5.0.0",
+    "javax.jms:jms-api:jar:1.1-rev-1",
+    "com.oracle:wlfullclient:jar:10.3.6.0",
+    "com.oracle:wlthint3client:jar:10.3.6.0",
+    "com.lihaoyi:ammonite-ops_2.11:jar:0.7.7",
+    "org.scala-lang.modules:scala-xml_2.11:jar:1.0.6",
+    "com.sun.xml.bind:jaxb-xjc:jar:2.2.11",
+    "com.sun.xml.bind:jaxb-impl:jar:2.2.11",
+    "com.sun.xml.bind:jaxb-core:jar:2.2.11",
+    "com.typesafe.slick:slick_2.11:jar:3.1.1",
+    "mysql:mysql-connector-java:jar:6.0.4",
+    "com.typesafe.slick:slick-codegen_2.11:jar:3.1.1",
+    "com.typesafe.slick:slick-hikaricp_2.11:jar:3.1.1",
+    "mysql:mysql-connector-java:jar:5.1.39",
+    "com.vividsolutions:jts-io:jar:1.14.0",
+    "com.h2database:h2:jar:1.4.192",
+    "com.github.tototoshi:slick-joda-mapper_2.11:jar:2.2.0",
+    "joda-time:joda-time:jar:2.9.4",
+    "org.joda:joda-convert:jar:1.8.1",
+    "com.badlogicgames.gdx:gdx-backend-lwjgl:jar:1.9.4",
+    "com.badlogicgames.gdx:gdx-platform:jar:natives-desktop:1.9.4",
+    "org.jgrapht:jgrapht-core:jar:1.0.0",
+    "com.github.yannrichet:JMathPlot:jar:1.0.1",
+    "ch.qos.logback:logback-classic:jar:1.1.7",
+    "osgi6:osgi6-api:jar:1.0.10",
+    "org.scala-lang:scala-compiler:jar:2.11.8"
+  )
+
+  val root = new File("../maven-modules/poms/src/main/scala/mvn")
+
+  def process(canonical: String) : Unit = {
+    val resolveds =
+      Maven
+        .resolver()
+        .resolve(canonical)
+        .withTransitivity()
+        .asResolvedArtifact()
+        .toSeq
+
+
+    val resolved +: resolvedDeps = resolveds
+
+    val dir = new File(
+      root,
+      s"${resolved.getCoordinate.getGroupId.replace('.', '/')}"
+    )
+    dir.mkdirs()
+
+    val fileName = s"${URLEncoder.encode(canonical, "UTF-8")}.scala"
+    val file = new File(dir, fileName)
+
+    val deps = resolvedDeps.filter(d => d.getScope != ScopeType.TEST && !d.isOptional)
+
+    if (!file.exists()) {
+
+      val content =
+        s"""
+           |package mvn
+           |
+           |object `${canonical}` extends _root_.jartree.util.CaseClassLoaderKey(
+           |  jar = _root_.jartree.util.MavenJarKeyImpl("${canonical}"),
+           |  dependenciesSeq = collection.immutable.Seq(
+           |    ${deps.map(d => s"`${d.getCoordinate.toCanonicalForm}`").mkString(",\n    ")}
+           |  )
+           |)
+         """.stripMargin
+
+      IO.write(
+        file,
+        content
+      )
+
+    }
+
+    deps.foreach { dep =>
+      process(dep.getCoordinate.toCanonicalForm)
+    }
+
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    root.mkdirs()
+//    IO.delete(root)
+    root.mkdirs()
+
+
+
+
+    artifacts.foreach({ canonical =>
+      try {
+        process(canonical)
+      } catch {
+        case NonFatal(ex) =>
+          ex.printStackTrace()
+      }
+    })
+
+  }
+
+}
