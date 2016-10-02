2277d33e5c8b4bee295baa5afcea3c2e77bfc48f
maprohu <maprohu@gmail.com>
2016-10-02 12:54:39 +0100

    maven modules

---
+++ poms/src/main/scala/mvn/com/oracle/com.oracle%3Awlfullclient%3Ajar%3A12.1.3.0.scala
@@ -0,0 +1,10 @@
+
+package mvn.com.oracle
+
+object `com.oracle:wlfullclient:jar:12.1.3.0` extends _root_.jartree.util.CaseClassLoaderKey(
+  jar = _root_.jartree.util.MavenJarKeyImpl("com.oracle:wlfullclient:jar:12.1.3.0"),
+  dependenciesSeq = collection.immutable.Seq(
+    
+  )
+)
+         
