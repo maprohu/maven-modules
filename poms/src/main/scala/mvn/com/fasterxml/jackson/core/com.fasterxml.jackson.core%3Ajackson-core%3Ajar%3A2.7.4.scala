eb84162d6ab0b8557e5982d6297d6a0e81e571ab
maprohu <maprohu@gmail.com>
2016-10-04 18:39:36 +0100

    vdm2cdf

---
+++ poms/src/main/scala/mvn/com/fasterxml/jackson/core/com.fasterxml.jackson.core%3Ajackson-core%3Ajar%3A2.7.4.scala
@@ -0,0 +1,7 @@
+
+package mvn
+
+object `com.fasterxml.jackson.core:jackson-core:jar:2.7.4` extends _root_.maven.modules.utils.MavenCentralModule(
+  "com.fasterxml.jackson.core:jackson-core:jar:2.7.4"
+)
+         
---
+++ poms/src/main/scala/mvn/com/fasterxml/jackson/core/com.fasterxml.jackson.core%3Ajackson-databind%3Ajar%3A2.7.4.scala
@@ -0,0 +1,9 @@
+
+package mvn
+
+object `com.fasterxml.jackson.core:jackson-databind:jar:2.7.4` extends _root_.maven.modules.utils.MavenCentralModule(
+  "com.fasterxml.jackson.core:jackson-databind:jar:2.7.4",
+  `com.fasterxml.jackson.core:jackson-annotations:jar:2.7.0`,
+  `com.fasterxml.jackson.core:jackson-core:jar:2.7.4`
+)
+         
