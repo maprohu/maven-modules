a96cd0d9b7e9186edc0ff621e82984b683d69c2f
maprohu <maprohu@gmail.com>
2016-10-11 12:41:03 +0100

    vdm2cdf

---
+++ poms/src/main/scala/mvn/io/monix/io.monix%3Amonix-types_2.11%3Ajar%3A2.0.4.scala
@@ -0,0 +1,8 @@
+
+package mvn
+
+object `io.monix:monix-types_2.11:jar:2.0.4` extends _root_.maven.modules.utils.MavenCentralModule(
+  "io.monix:monix-types_2.11:jar:2.0.4",
+  `org.scala-lang:scala-library:jar:2.11.8`
+)
+         
