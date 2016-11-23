bb37433c06e67a6f4c3f5bac0cd3f00dbc97d660
maprohu <maprohu@gmail.com>
2016-11-23 18:20:22 +0000

    voice mary

---
+++ builder/src/main/scala/mvn/com/jssrc/com.jssrc%3Ajssrc%3Ajar%3A1.0.1.scala
@@ -0,0 +1,7 @@
+
+package mvn
+
+object `com.jssrc:jssrc:jar:1.0.1` extends _root_.mvnmod.builder.MavenCentralModule(
+  "com.jssrc:jssrc:jar:1.0.1"
+)
+         
--- generator/src/main/scala/mvnmod/generator/RunGenerateMaven.scala
+++ generator/src/main/scala/mvnmod/generator/RunGenerateMaven.scala
@@ -164,7 +164,8 @@ object RunGenerateMaven {
     "org.docx4j:docx4j-ImportXHTML:jar:3.3.1",
     "javax.ejb:ejb-api:jar:3.0",
     "org.springframework:spring-jdbc:jar:4.1.9.RELEASE",
-    "org.littleshoot:littleproxy:jar:1.1.0"
+    "org.littleshoot:littleproxy:jar:1.1.0",
+    "com.jssrc:jssrc:jar:1.0.1"
 
   //    "android-api:android-api:jar:22",
 //    "org.macroid:macroid_2.11:aar:2.0.0-M5"
