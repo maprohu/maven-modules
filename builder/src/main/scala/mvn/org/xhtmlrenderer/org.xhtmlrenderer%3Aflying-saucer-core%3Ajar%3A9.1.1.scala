5b84ea64010f6c42edf62323879632cd5c662396
maprohu <maprohu@gmail.com>
2017-01-20 12:16:42 +0000

    cluster

---
+++ builder/src/main/scala/mvn/org/xhtmlrenderer/org.xhtmlrenderer%3Aflying-saucer-core%3Ajar%3A9.1.1.scala
@@ -0,0 +1,7 @@
+
+package mvn
+
+object `org.xhtmlrenderer:flying-saucer-core:jar:9.1.1` extends _root_.mvnmod.builder.MavenCentralModule(
+  "org.xhtmlrenderer:flying-saucer-core:jar:9.1.1"
+)
+         
---
+++ builder/src/main/scala/mvn/org/xhtmlrenderer/org.xhtmlrenderer%3Aflying-saucer-pdf%3Ajar%3A9.1.1.scala
@@ -0,0 +1,14 @@
+
+package mvn
+
+object `org.xhtmlrenderer:flying-saucer-pdf:jar:9.1.1` extends _root_.mvnmod.builder.MavenCentralModule(
+  "org.xhtmlrenderer:flying-saucer-pdf:jar:9.1.1",
+  `com.lowagie:itext:jar:2.1.7`,
+  `bouncycastle:bcmail-jdk14:jar:138`,
+  `bouncycastle:bcprov-jdk14:jar:138`,
+  `org.bouncycastle:bctsp-jdk14:jar:1.38`,
+  `org.bouncycastle:bcprov-jdk14:jar:1.38`,
+  `org.bouncycastle:bcmail-jdk14:jar:1.38`,
+  `org.xhtmlrenderer:flying-saucer-core:jar:9.1.1`
+)
+         
