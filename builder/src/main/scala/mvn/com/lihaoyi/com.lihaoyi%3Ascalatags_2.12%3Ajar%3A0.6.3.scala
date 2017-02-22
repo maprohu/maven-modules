bf3c596a245c74a6fd92fc43821d953e6b4c8970
maprohu <maprohu@gmail.com>
2017-02-22 17:32:34 +0000

    organizer

---
+++ builder/src/main/scala/mvn/com/lihaoyi/com.lihaoyi%3Ascalatags_2.12%3Ajar%3A0.6.3.scala
@@ -0,0 +1,9 @@
+
+package mvn
+
+object `com.lihaoyi:scalatags_2.12:jar:0.6.3` extends _root_.mvnmod.builder.MavenCentralModule(
+  "com.lihaoyi:scalatags_2.12:jar:0.6.3",
+  `org.scala-lang:scala-library:jar:2.12.0`,
+  `com.lihaoyi:sourcecode_2.12:jar:0.1.3`
+)
+         
