38e76109a62c3ecadbb0d7a2861da4c45d2cb340
maprohu <maprohu@gmail.com>
2017-04-20 10:47:03 +0100

    handover ftx

---
+++ builder/src/main/scala/mvn/com/lihaoyi/com.lihaoyi%3Aammonite-ops_2.12%3Ajar%3A0.8.2.scala
@@ -0,0 +1,9 @@
+
+package mvn
+
+object `com.lihaoyi:ammonite-ops_2.12:jar:0.8.2` extends _root_.mvnmod.builder.MavenCentralModule(
+  "com.lihaoyi:ammonite-ops_2.12:jar:0.8.2",
+  `org.scala-lang:scala-library:jar:2.12.1`,
+  `com.lihaoyi:geny_2.12:jar:0.1.1`
+)
+         
